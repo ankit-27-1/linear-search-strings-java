6a459fe3ae4318c05500482d13bb3834ecfd04d6
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-05-10 23:56:00 +0530

    Create searchstr.java

---
+++ searchstr.java
@@ -0,0 +1,16 @@
+public class searchstr{
+    public static void main(String[] args) {
+        String name="Ankit";
+        System.out.println(search(name,'k'));
+    }
+    static boolean search(String str,char target){
+        if(str.length()==0){
+            return false;
+        }
+        for(int i=0;i<str.length();i++){
+            if(target==str.charAt(i));
+            return true;
+        }
+        return false;
+    }
+}
